c6629d67e7911b4c160e9f22e2d2f25e34c3b723
HyemIin <hyem5019@gmail.com>
2024-02-16 14:28:28 +0900

    Feat : 백준 ID별 맞춘 문제 리스트 크롤링
    - 백준 웹사이트 크롤링 진행

--- src/main/java/com/hyem/baekjoon_random_defense/domain/exam/controller/ExamController.java
+++ src/main/java/com/hyem/baekjoon_random_defense/domain/exam/controller/ExamController.java
@@ -1,4 +1,24 @@
 package com.hyem.baekjoon_random_defense.domain.exam.controller;
 
+import com.hyem.baekjoon_random_defense.domain.exam.dto.request.RequestCorrectExamDTO;
+import com.hyem.baekjoon_random_defense.domain.exam.service.BaekJoonCrwalService;
+import com.hyem.baekjoon_random_defense.domain.global.api.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class ExamController {
+    private final BaekJoonCrwalService baekJoonCrwalService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> getCorrectExam(@RequestBody RequestCorrectExamDTO requestCorrectExamDTO) {
+        return ResponseEntity
+            .ok()
+            .body(ApiUtils.success(baekJoonCrwalService.getCorrectExamNumber(requestCorrectExamDTO)));
+    }
+
 }
--- src/main/java/com/hyem/baekjoon_random_defense/domain/exam/service/ExamService.java
+++ src/main/java/com/hyem/baekjoon_random_defense/domain/exam/service/ExamService.java
@@ -1,4 +1,11 @@
 package com.hyem.baekjoon_random_defense.domain.exam.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ExamService {
+//    private ExamRepository examRepository;
+
 }
