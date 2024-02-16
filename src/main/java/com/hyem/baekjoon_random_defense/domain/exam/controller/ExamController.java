63582318a0a1a6bf363484c44840b0508fb2cfb5
HyemIin <hyem5019@gmail.com>
2024-02-16 23:29:57 +0900

    Feat : 백준 티어별 문제 리스트 크롤링
    - 브론즈,실버 티어 enum값으로 정리

--- src/main/java/com/hyem/baekjoon_random_defense/domain/exam/controller/ExamController.java
+++ src/main/java/com/hyem/baekjoon_random_defense/domain/exam/controller/ExamController.java
@@ -2,23 +2,30 @@ package com.hyem.baekjoon_random_defense.domain.exam.controller;
 
 import com.hyem.baekjoon_random_defense.domain.exam.dto.request.RequestCorrectExamDTO;
 import com.hyem.baekjoon_random_defense.domain.exam.service.BaekJoonCrwalService;
+import com.hyem.baekjoon_random_defense.domain.exam.service.ExamService;
 import com.hyem.baekjoon_random_defense.domain.global.api.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ExamController {
     private final BaekJoonCrwalService baekJoonCrwalService;
+    private final ExamService examService;
 
     @PostMapping("/")
     public ResponseEntity<?> getCorrectExam(@RequestBody RequestCorrectExamDTO requestCorrectExamDTO) {
         return ResponseEntity
             .ok()
             .body(ApiUtils.success(baekJoonCrwalService.getCorrectExamNumber(requestCorrectExamDTO)));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getRandomExam(@RequestParam(name = "tier") String tier) {
+        return ResponseEntity
+            .ok()
+            .body(ApiUtils.success(examService.getRamdomExam(tier)));
     }
 
 }
--- src/main/java/com/hyem/baekjoon_random_defense/domain/exam/service/ExamService.java
+++ src/main/java/com/hyem/baekjoon_random_defense/domain/exam/service/ExamService.java
@@ -1,11 +1,30 @@
 package com.hyem.baekjoon_random_defense.domain.exam.service;
 
+import com.hyem.baekjoon_random_defense.domain.exam.type.Tier;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class ExamService {
-//    private ExamRepository examRepository;
+    private final String URL = "https://www.acmicpc.net/problemset?sort=no_asc&tier=";
+
+    public Document getRamdomExam(String tier) {
+        String selectedTier = Tier.valueOf(tier).getTier();
+        Document document = null;
+        try {
+            document = Jsoup.connect(URL+selectedTier).get();
+            System.out.println(document);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Elements exam = document.select("div.problem-list a");
+        return document;
+    }
 
 }
