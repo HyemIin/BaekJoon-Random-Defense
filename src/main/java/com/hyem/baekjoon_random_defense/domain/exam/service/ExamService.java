7bfd8a2f2dde0b0c20df5758d8f80e419a98bfeb
HyemIin <hyem5019@gmail.com>
2024-02-17 00:58:33 +0900

    Feat : 백준 티어별 문제 랜덤 출제
    - 브론즈,실버 티어 랜덤 출제 로직 생성

--- src/main/java/com/hyem/baekjoon_random_defense/domain/exam/service/ExamService.java
+++ src/main/java/com/hyem/baekjoon_random_defense/domain/exam/service/ExamService.java
@@ -4,27 +4,45 @@ import com.hyem.baekjoon_random_defense.domain.exam.type.Tier;
 import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class ExamService {
     private final String URL = "https://www.acmicpc.net/problemset?sort=no_asc&tier=";
 
-    public Document getRamdomExam(String tier) {
+    public String getRamdomExam(String tier) {
         String selectedTier = Tier.valueOf(tier).getTier();
-        Document document = null;
+        List ExamCandidateList = new ArrayList<>();
         try {
-            document = Jsoup.connect(URL+selectedTier).get();
-            System.out.println(document);
+            //티어별 문제 리스트 특정 페이지 가져오기
+            Document document = Jsoup.connect(URL+selectedTier).get();
+            Elements pageList = document.select("div.text-center ul.pagination li");
+            int randomPageNumber = new Random().nextInt(pageList.size());
+            Document selectedPageUrl = Jsoup.connect(URL+selectedTier+"&page="+randomPageNumber).get();
+            //위에서 가져온 페이지에 포함된 문제번호 파싱
+            Elements selectedExam = selectedPageUrl.select("td.list_problem_id");
+            for (Element element : selectedExam) {
+                String parsedElement = element.text();
+                String ExamNumber = parsedElement.replaceAll("[^0-9]", "");
+                ExamCandidateList.add(ExamNumber);
+            }
+            String Exam;
+            for (Object examNumber : ExamCandidateList) {
+                return "https://www.acmicpc.net/problem/" + examNumber;
+            }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Elements exam = document.select("div.problem-list a");
-        return document;
+        return selectedTier;
     }
 
 }
